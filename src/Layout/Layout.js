15490478a40a309f1f4fa6408b5db45501d7f8e9
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-14 16:36:59 +0530

    Updates in event page

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,31 +1,31 @@
-import React from "react";
-import classes from "../Style/layout.module.css";
-import Navi from "../components/Navi";
+import React from 'react'
+import classes from '../Style/layout.module.css'
+import Navi from '../components/Navi'
 
 const Layout = (props) => {
-  if (props.mode == "1") {
+  if (props.mode == '1') {
     return (
       <div className={classes.layoutcont}>
-        <div className={[classes.datacont, classes.rightal].join(" ")}>
+        <div className={[classes.datacont, classes.rightal].join(' ')}>
           {props.children}
         </div>
-        <div className={[classes.navcont, classes.leftal].join(" ")}>
-          <Navi />
+        <div className={[classes.navcont, classes.leftal].join(' ')}>
+          <Navi mode={props.mode} />
         </div>
       </div>
-    );
+    )
   } else {
     return (
       <div className={classes.layoutcont}>
-        <div className={[classes.datacont, classes.leftal].join(" ")}>
+        <div className={[classes.datacont, classes.leftal].join(' ')}>
           {props.children}
         </div>
-        <div className={[classes.navcont, classes.rightal].join(" ")}>
-          <Navi />
+        <div className={[classes.navcont, classes.rightal].join(' ')}>
+          <Navi mode={props.mode} />
         </div>
       </div>
-    );
+    )
   }
-};
+}
 
-export default Layout;
+export default Layout
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import classes from '../Style/events.module.css'
+import Layout from '../Layout/Layout'
 
 const Events = () => {
-  return <div></div>
+  return (
+    <div>
+      <Layout mode="0"></Layout>
+    </div>
+  )
 }
 
 export default Events
