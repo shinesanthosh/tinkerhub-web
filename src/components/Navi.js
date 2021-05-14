bdfac7f1744d0d0bb9a032eb0cabce18d4830034
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-14 16:36:16 +0530

    Added navigation for sub pages

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,26 +1,44 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
 
-import classes from "../Style/navi.module.css";
+import classes from '../Style/navi.module.css'
 
 const Navi = (props) => {
-  return (
-    <div>
-      <div className={classes.navbox}>
-        <Link href="#">
-          <a>EVENTS</a>
-        </Link>
-        <Link href="#">
-          <a>MATERIALS</a>
-        </Link>
-        <Link href="#">
-          <a>SOCIAL</a>
-        </Link>
+  if (props.mode == '1') {
+    return (
+      <div>
+        <div className={classes.navbox}>
+          <Link href="/events">
+            <a>EVENTS</a>
+          </Link>
+          <Link href="#">
+            <a>MATERIALS</a>
+          </Link>
+          <Link href="#">
+            <a>SOCIAL</a>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    )
+  } else {
+    return (
+      <div>
+        <div className={classes.navbox2}>
+          <Link href="/events">
+            <a>EVENTS</a>
+          </Link>
+          <Link href="#">
+            <a>MATERIALS</a>
+          </Link>
+          <Link href="#">
+            <a>SOCIAL</a>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
-Navi.propTypes = {};
+Navi.propTypes = {}
 
-export default Navi;
+export default Navi
