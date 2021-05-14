39d708a1c8763cd05476468f2c54e08075b5dbeb
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-14 22:43:04 +0530

    Added title for the events page

--- src/pages/events.js
+++ src/pages/events.js
@@ -5,7 +5,11 @@ import Layout from '../Layout/Layout'
 const Events = () => {
   return (
     <div>
-      <Layout mode="0" page="1"></Layout>
+      <Layout mode="0" page="1">
+        <div className={classes.cont}>
+          <p className={classes.title}>UPCOMING</p>
+        </div>
+      </Layout>
     </div>
   )
 }
