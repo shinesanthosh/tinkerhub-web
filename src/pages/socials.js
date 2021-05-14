ad1b5113f15dc6add4879dc854c660e1d5ad6c34
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-15 01:00:45 +0530

    added layout to social page

--- src/pages/socials.js
+++ src/pages/socials.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
+
 import classes from '../Style/socials.module.css'
 import Layout from '../Layout/Layout'
 
 const Events = () => {
   return (
     <div>
-      <Layout mode="0" page="3"></Layout>
+      <Layout mode="0" page="3">
+        <div className={classes.cont}>
+          <p className={classes.title}>OUR SOCIALS</p>
+        </div>
+      </Layout>
     </div>
   )
 }
