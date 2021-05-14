055c3c2e20c31200b34693319d9572f3c5ffcd60
shinesanthosh <shinesanthosh2000@gmail.com>
2021-05-15 00:39:09 +0530

    Added lists in events

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link'
+
 import classes from '../Style/events.module.css'
 import Layout from '../Layout/Layout'
 
@@ -8,6 +10,20 @@ const Events = () => {
       <Layout mode="0" page="1">
         <div className={classes.cont}>
           <p className={classes.title}>UPCOMING</p>
+          <ul className={classes.list}>
+            <li>
+              10 Days of Python
+              <Link href="#">
+                <a>REGISTER</a>
+              </Link>
+            </li>
+            <li>
+              How to Git?
+              <Link href="#">
+                <a>REGISTER</a>
+              </Link>
+            </li>
+          </ul>
         </div>
       </Layout>
     </div>
