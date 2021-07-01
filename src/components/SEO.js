d48326f953479758af9461a064a64b89a813e9aa
shinesanthosh <shinesanthosh2000@gmail.com>
2021-07-01 17:32:18 +0530

    This ain't gonna fix soon

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -27,6 +27,12 @@ const MetData = (props) => {
         itemprop="image"
         content="https://tinkerhubpce.vercel.app/icons/logo_icon.jpg"
       />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:image:type" content="image/jpeg" />
+
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="300" />
       <meta property="og:type" content="website" />
     </Head>
   )
