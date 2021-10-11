6cc5be904f093367dfac455ef0d52c8c6dab7cb3
shinesanthosh <shinesanthosh2000@gmail.com>
2021-10-11 22:02:52 +0530

    Pacakage mosmatch fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../Style/main.css'
 import Head from 'next/head'
 
-export default function MyApp({Component, pageProps}) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
